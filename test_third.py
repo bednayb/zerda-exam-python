d7ae88857eb55d7736b664f186bce0c39f03e2fd
bednayb <bednaybence@gmail.com>
2016-12-05 09:44:50 +0100

    test_third is ready

---
+++ test_third.py
@@ -0,0 +1,18 @@
+import third
+import unittest
+
+class Test_your_name_work(unittest.TestCase):
+
+
+
+    def test_string(self):
+        self.assertEqual(third.count_letter_in_string('aniko',"o"), 1)
+
+    def test_not_string_input(self):
+        self.assertRaises(TypeError, third.count_letter_in_string, 3)
+
+    def test_Uppercase_or_Lowercase(self):
+        self.assertEqual(third.count_letter_in_string('Aniko',"a"), 0)
+
+if __name__ == '__main__':
+    unittest.main()
