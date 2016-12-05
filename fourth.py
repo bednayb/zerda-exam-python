519ad434b4c1e888cbf7f4c2afd7a3564423a4de
bednayb <bednaybence@gmail.com>
2016-12-05 10:29:01 +0100

    problem with falcon1

--- fourth.py
+++ fourth.py
@@ -23,10 +23,10 @@
 # The following code should work with the class:
 
 class Rocket():
-    def __init__(self, type_of_rocket):
+    def __init__(self, type_of_rocket,fuel_level = 0, launches = 0):
         self.type_of_rocket = type_of_rocket
-        self.fuel_level = 0
-        self.launches = 0
+        self.fuel_level = fuel_level
+        self.launches = launches
 
     def launch(self):
         if self.type_of_rocket == "falcon1":
@@ -38,25 +38,32 @@ class Rocket():
                 self.launches += 1
                 self.fuel_level -= 9
 
-
-
-
     def refill(self):
-        pass
+        if self.type_of_rocket == "falcon1":
+            if self.fuel_level >= 5:
+                return 5
+            else:
+                return 5 - self.fuel_level
+
+        elif self.type_of_rocket == "falcon9":
+            if self.fuel_level >= 20:
+                return 5
+            else:
+                return 20 - self.fuel_level
 
     def getStats(self):
-        pass
+        return "name:", self.type_of_rocket, "fuel:", self.fuel_level,"launches:", self.launches
 
 
 
 
 
 
-    falcon1 = Rocket('falcon1')
-    returned_falcon9 = Rocket('falcon9', 11, 1)
+falcon1 = Rocket('falcon1')
+returned_falcon9 = Rocket('falcon9', 11, 1)
 
-    falcon1.refill() # 5
-    falcon1.launch()
+falcon1.refill() # 5
+falcon1.launch()
 
-    print(falcon1.getStats()) # name: falcon1, fuel: 4, launches: 1
-    print(returned_falcon9.getStats()) # name: falcon9, fuel: 11, launches: 1
+print(falcon1.getStats()) # name: falcon1, fuel: 4, launches: 1
+print(returned_falcon9.getStats()) # name: falcon9, fuel: 11, launches: 1
