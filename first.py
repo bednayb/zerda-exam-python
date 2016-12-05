51c41aa99d6faa98af0fe95a11ec07d0bfc720a3
bednayb <bednaybence@gmail.com>
2016-12-05 09:22:14 +0100

    first task is ready:

--- first.py
+++ first.py
@@ -2,3 +2,23 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+list_for_function =[1,2,3,4,5]
+
+### FUNCTION ###
+def list_doubled(list_simple):
+    new_list = []
+    ### CHECK THE TYPE ###
+    if isinstance(list_simple, list):
+        ### DOUBLE EVERY ELEMENT ###
+        for i in range(len(list_simple)):
+            if i%2 == 1:
+                new_list.append(list_simple[i])
+        return new_list
+    else:
+        ### ERROR FOR NOT LIST ###
+        raise TypeError("it's not a list")
+
+### CALL THE FUNCTION ###
+list_doubled(list_for_function)
+# list_doubled(5)
